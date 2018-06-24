5d1dc18997e34442c72e3c88791109c0c4270d68
Bruno Furtado <brunofurtadoc@gmail.com>
2018-06-24 18:06:10 -0300

    Add a couple themes

--- elisp/custom-ui.el
+++ elisp/custom-ui.el
@@ -1,5 +1,25 @@
+;;; Disable toolbar & menubar & scroll-bar
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; Themes
+(use-package flatland-black-theme
+  :ensure t)
+
+;; (use-package doom-themes
+;;   :ensure t
+;;   :config
+;;   (setq doom-themes-enable-bold t
+;;         doom-themes-enable-italic t)
+;;   :init
+;;   (load-theme 'doom-one t)
+;;   (doom-themes-visual-bell-config)
+;;   (doom-themes-org-config))
+
+;; (use-package monokai-theme
+;;   :ensure t
+;;   :init
+;;   (load-theme 'monokai t))
 
 (provide 'custom-ui)
