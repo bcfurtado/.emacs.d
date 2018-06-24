423df27a6b30e8096d887773a8982332478bf2cd
Bruno Furtado <brunofurtadoc@gmail.com>
2018-06-24 01:49:39 -0300

    Update default META Key on macOS and Ubuntu

--- init.el
+++ init.el
@@ -2,3 +2,13 @@
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+(defconst *is-a-mac* (eq system-type 'darwin))
+
+(when *is-a-mac*
+  (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier 'none)
+  (setq default-input-method "MacOSX")
+  )
+
+(setq x-super-keysym 'meta)
