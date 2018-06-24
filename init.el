1ac5556e7105994d7c9f5b1d5bce33961fe79664
Bruno Furtado <brunofurtadoc@gmail.com>
2018-06-24 03:21:54 -0300

    Add drag-stuff package

--- init.el
+++ init.el
@@ -30,3 +30,10 @@
 
 (use-package magit
   :bind ("C-x g" . magit-status))
+
+(use-package drag-stuff
+  :ensure t
+  :config
+  (drag-stuff-global-mode 1)
+  :bind* (("M-p" . drag-stuff-up)
+          ("M-n" . drag-stuff-down)))
