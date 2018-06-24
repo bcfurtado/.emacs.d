d9ce99747fded06f6e006cdbf2fab6f909774546
Bruno Furtado <brunofurtadoc@gmail.com>
2018-06-24 06:00:22 -0300

    Fix shadowed magit key bindings

--- elisp/custom-editor.el
+++ elisp/custom-editor.el
@@ -1,9 +1,7 @@
 (use-package drag-stuff
   :ensure t
-  :config
-  (drag-stuff-global-mode 1)
-  :bind* (("M-p" . drag-stuff-up)
-          ("M-n" . drag-stuff-down)))
+  :bind (("M-p" . drag-stuff-up)
+         ("M-n" . drag-stuff-down)))
 
 
 (provide 'custom-editor)
--- elisp/custom-vc.el
+++ elisp/custom-vc.el
@@ -1,8 +1,5 @@
 (use-package magit
   :ensure t
-  :bind (("C-x g" . magit-status)
-         ("M-p" . git-rebase-move-line-up)
-         ("M-n" . git-rebase-move-line-down))
-  )
+  :bind ("C-x g" . magit-status))
 
 (provide 'custom-vc)
