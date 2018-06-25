07f24876bb4f61d345c38650f03ef56bf4ba74ef
Bruno Furtado <brunofurtadoc@gmail.com>
2018-06-25 00:10:21 -0300

    Enable Delete Selection mode

--- elisp/custom-setup.el
+++ elisp/custom-setup.el
@@ -9,3 +9,10 @@
 (setq x-super-keysym 'meta)
 
 (provide 'custom-setup)
+
+
+;; If you enable Delete Selection mode, a minor mode, then inserting
+;; text while the mark is active causes the selected text to be
+;; deleted first. This also deactivates the mark. Many graphical
+;; applications follow this convention, but Emacs does not.
+(delete-selection-mode t)
