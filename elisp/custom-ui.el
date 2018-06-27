6d1882a10116553751c6e29c79e486c4f902c2b3
Bruno Furtado <brunofurtadoc@gmail.com>
2018-06-27 12:03:38 -0300

    Update theme. Use monokai theme.

--- elisp/custom-ui.el
+++ elisp/custom-ui.el
@@ -4,8 +4,8 @@
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 ;; Themes
-(use-package flatland-black-theme
-  :ensure t)
+;; (use-package flatland-black-theme
+;;   :ensure t)
 
 ;; (use-package doom-themes
 ;;   :ensure t
@@ -17,10 +17,10 @@
 ;;   (doom-themes-visual-bell-config)
 ;;   (doom-themes-org-config))
 
-;; (use-package monokai-theme
-;;   :ensure t
-;;   :init
-;;   (load-theme 'monokai t))
+(use-package monokai-theme
+  :ensure t
+  :init
+  (load-theme 'monokai t))
 
 
 ;;; Define default font size for the frames
