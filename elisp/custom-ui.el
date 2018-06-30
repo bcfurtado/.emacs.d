6cfdac47dedffe4f15258a4157fc0b4aed92c329
Bruno Furtado <brunofurtadoc@gmail.com>
2018-06-30 13:33:38 -0300

    Always create new frames on maximized state

--- elisp/custom-ui.el
+++ elisp/custom-ui.el
@@ -30,6 +30,14 @@
 (set-default-font-zize (selected-frame))
 (add-hook 'after-make-frame-functions 'set-default-font-zize)
 
+;;; Open new frames always maximized
+(defun set-frame-maximized (&optional frame)
+  (set-frame-parameter frame 'fullscreen 'maximized))
+
+(add-hook 'window-setup-hook 'set-frame-maximized)
+(add-hook 'after-make-frame-functions 'set-frame-maximized)
+
+
 
 ;; This package allow us hide or abbreviate the mode line displays of
 ;; minor-modes.
