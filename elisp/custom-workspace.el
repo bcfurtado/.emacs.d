fad6d4ffaef644deb255dac8d0e2ddb9c2fcf746
Bruno Furtado <brunofurtadoc@gmail.com>
2018-07-01 12:55:30 -0300

    Use ivy instead of ido on projectile

--- elisp/custom-workspace.el
+++ elisp/custom-workspace.el
@@ -1,8 +1,8 @@
 (use-package projectile
   :ensure t
   :init
-  (projectile-global-mode t))
-
+  (projectile-global-mode t)
+  (setq projectile-completion-system 'ivy))
 
 
 (provide 'custom-workspace)
