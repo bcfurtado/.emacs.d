4465ef377408c5747d40c03b08e5b0cf57eeb1e2
Bruno Furtado <brunofurtadoc@gmail.com>
2018-07-01 22:21:40 -0300

    Add diff-hl package

--- elisp/custom-vc.el
+++ elisp/custom-vc.el
@@ -7,4 +7,16 @@
   :ensure t
   :bind ("C-c v t" . git-timemachine))
 
+(use-package diff-hl
+  :ensure t
+  :demand t
+  :bind (("C-c v p" . diff-hl-previous-hunk)
+         ("C-c v n" . diff-hl-next-hunk)
+         ("C-c v r" . diff-hl-revert-hunk))
+  :config
+    (diff-hl-flydiff-mode +1)
+    (global-diff-hl-mode +1)
+    (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
+    (add-hook 'dired-mode-hook 'diff-hl-dired-mode-unless-remote))
+
 (provide 'custom-vc)
