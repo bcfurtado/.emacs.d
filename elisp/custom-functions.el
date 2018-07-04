f8b46b6519ce502b07cd75b57a202c209f413af2
Bruno Furtado <brunofurtadoc@gmail.com>
2018-07-04 17:33:51 -0300

    Add copy-file-name-to-clipboard function
    Allow us to have the full file path.

--- elisp/custom-functions.el
+++ elisp/custom-functions.el
@@ -4,4 +4,14 @@
      (interactive)
      ,@body))
 
+(defun copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
+
 (provide 'custom-functions)
--- elisp/custom-keybinds.el
+++ elisp/custom-keybinds.el
@@ -1,4 +1,5 @@
 ;; Instant Access to init.el
 (global-set-key (kbd "<f8>") (λ (find-file-other-window (expand-file-name "init.el" user-emacs-directory))))
+(global-set-key (kbd "C-c c") 'copy-file-name-to-clipboard)
 
 (provide 'custom-keybinds)
