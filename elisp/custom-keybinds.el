aad5e360ae25af4583baf026198dd32cb5e11ea7
Bruno Furtado <brunofurtadoc@gmail.com>
2018-07-10 21:06:34 -0300

    Add browser-url and toggle-truncate-lines keybinds

--- elisp/custom-keybinds.el
+++ elisp/custom-keybinds.el
@@ -1,7 +1,9 @@
 ;; Instant Access to init.el
 (global-set-key (kbd "<f8>") (λ (find-file-other-window (expand-file-name "init.el" user-emacs-directory))))
 (global-set-key (kbd "<f7>") 'bc/test-django-function)
+(global-set-key (kbd "<f1>") 'toggle-truncate-lines)
 (global-set-key (kbd "C-c c") 'copy-file-name-to-clipboard)
+(global-set-key (kbd "C-c M-o") 'browse-url)
 
 ;; Naming
 (global-set-key (kbd "C-c m -") (λ (replace-region-by 's-dashed-words)))
