ff7426368c1e07354afd87cb9b177bcc0772d393
Bruno Furtado <brunofurtadoc@gmail.com>
2018-12-31 05:28:27 +0100

    Add Java packages as experimental

--- elisp/custom-experimental.el
+++ elisp/custom-experimental.el
@@ -8,4 +8,23 @@
   :config
   (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode)))
 
+
+;; Java support
+(use-package treemacs :ensure t)
+(use-package yasnippet :ensure t)
+(use-package hydra :ensure t)
+(use-package lsp-java
+  :ensure t
+  :after lsp
+  :config (add-hook 'java-mode-hook 'lsp))
+
+(use-package dap-mode
+  :ensure t :after lsp-mode
+  :config
+  (dap-mode t)
+  (dap-ui-mode t))
+
+(use-package dap-java :after (lsp-java))
+(use-package lsp-java-treemacs :after (treemacs))
+
 (provide 'custom-experimental)
