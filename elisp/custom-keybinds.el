8773df60d34f14056dd872b4f4518ec2f11cd581
Bruno Furtado <brunofurtadoc@gmail.com>
2019-05-14 21:51:20 +0200

    Add shortcut to quickly open a file path at buffer position

--- elisp/custom-keybinds.el
+++ elisp/custom-keybinds.el
@@ -2,6 +2,7 @@
 (global-set-key (kbd "<f9>") 'bc/insert-jira-task-id)
 (global-set-key (kbd "<f8>") (λ (find-file-other-window (expand-file-name "init.el" user-emacs-directory))))
 (global-set-key (kbd "<f1>") 'toggle-truncate-lines)
+(global-set-key (kbd "<f2>") 'find-file-at-point)
 (global-set-key (kbd "C-c c") 'copy-file-name-to-clipboard)
 (global-set-key (kbd "C-c M-o") 'browse-url)
 
