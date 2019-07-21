03dcca3f861e3ed768f6861056094cf8eacb8888
Bruno Furtado <brunofurtadoc@gmail.com>
2019-07-21 03:14:11 +0200

    Rename django-test functions and add documentation

--- elisp/django-test.el
+++ elisp/django-test.el
@@ -13,30 +13,41 @@
 ;;; Code:
 
 (require 'which-func)
-(defun bc/current-project-folder ()
+
+
+(defun django-test-project-folder ()
+  "Return Django root project path.
+Currently, we are assuming that the root folder is the one that
+contains the git folder."
   (locate-dominating-file (buffer-file-name) ".git"))
 
-(defun bc/current-file-name-in-the-project ()
-  (file-relative-name (buffer-file-name) (bc/current-project-folder)))
+(defun django-test-file-path ()
+  "Return the path of the file to be tested relative to the project root directory."
+  (file-relative-name (buffer-file-name) (django-test-project-folder)))
 
-(defun bc/get-module-name ()
-  (let* ((path-file (bc/current-file-name-in-the-project))
+(defun django-test-python-module-from-file-path ()
+  "Return the current python module based on file path."
+  (let* ((path-file (django-test-file-path))
          (module-name (substring path-file 0 (string-match ".py" path-file))))
     (replace-regexp-in-string "/" "." module-name)))
 
-(defun bc/test-django-function ()
+(defun django-test-run-test-at-point ()
+  "Run django test at the point.
+Invoke this function and you'll be promoted with the exact command to
+run only the tests for the specific file.
+Keep your cursor under the class name or the function and the command
+will be even more specific."
   (interactive)
-
-  (let* ((module (bc/get-module-name))
+  (let* ((module (django-test-python-module-from-file-path))
          (func (which-function))
          (command (concat "python manage.py test " module "." func " --no-input")))
     (save-excursion
       (setq compilation-read-command t)
-      (setq project-root-folder (find-file-noselect (bc/current-project-folder)))
+      (setq project-root-folder (find-file-noselect (django-test-project-folder)))
       (set-buffer project-root-folder)
       (setq compile-command command)
       (call-interactively 'compile)
       (kill-buffer project-root-folder))))
 
 (provide 'django-test)
-;;; django-test.el ends here
+;;; django-test ends here
