6302617cbf6c1273fcccbeafe90ce5d7edaccce6
Bruno Furtado <brunofurtadoc@gmail.com>
2019-07-29 00:33:32 +0200

    Add straight.el

--- elisp/custom-pm-setup.el
+++ elisp/custom-pm-setup.el
@@ -18,5 +18,19 @@
 
 (require 'use-package)
 
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 (provide 'custom-pm-setup)
 ;;; custom-pm-setup.el ends here
