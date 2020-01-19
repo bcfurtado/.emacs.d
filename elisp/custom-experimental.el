1660c76a9f598cb8b2c7b9f6c10176ec5a1967be
Bruno Furtado <brunofurtadoc@gmail.com>
2020-01-19 21:40:33 +0100

    Add highlight-indent-guides package

--- elisp/custom-experimental.el
+++ elisp/custom-experimental.el
@@ -67,4 +67,10 @@
 (use-package deadgrep
   :ensure t)
 
+(use-package highlight-indent-guides
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
+  (setq highlight-indent-guides-method 'character))
+
 (provide 'custom-experimental)
--- elisp/custom-org-mode.el
+++ elisp/custom-org-mode.el
@@ -21,6 +21,7 @@
 (use-package yaml-mode
   :ensure t
   :config
-  (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode)))
+  (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+  (add-hook 'yaml-mode-hook 'highlight-indent-guides-mode))
 
 (provide 'custom-org-mode)
