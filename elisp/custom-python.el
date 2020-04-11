6f5e3bb7ef7f975b59197cfe9cb05e416fcbd3fc
Bruno Furtado <brunofurtadoc@gmail.com>
2020-04-11 13:28:50 +0200

    Add yapfify package

--- elisp/custom-python.el
+++ elisp/custom-python.el
@@ -5,6 +5,9 @@
 
 (require 'django-test-runner)
 
+(use-package yapfify
+  :ensure t)
+
 (use-package python
   :init
   (require 'python)
@@ -15,6 +18,8 @@
           ("<f10>" . django-test-runner)
           ("C-M-f" . sp-forward-sexp)
           ("C-M-b" . sp-backward-sexp)
+          ("C-c C-t f" . yapfify-region)
+          ("C-c C-t F" . yapfify-buffer)
           ("C-c C-t o" . py-isort-buffer))
   :config
   (add-hook 'python-mode-hook #'auto-virtualenv-set-virtualenv))
